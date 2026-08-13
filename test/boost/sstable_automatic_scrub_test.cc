c2350b32b7df4fa54f445c4ffd59fe1380c6dc26
Jakub Czyszczoń <jakub.czyszczon@scylladb.com>
2026-08-13 16:36:26 +0200

    test/boost: add test for automatic scrub with invalid scylla sst
    Add a test, which checks whether automatic scrub correctly finds
    corruption in sstables with a scylla component and sets the scrub time.
    
    Add `wait_on_enter` helper to wait for a number of times a given error
    injection is reached.

--- test/boost/sstable_automatic_scrub_test.cc
+++ test/boost/sstable_automatic_scrub_test.cc
@@ -124,4 +124,43 @@ public:
     }
 };
 
+future<> wait_on_enter(std::string_view name, size_t count = 1, std::chrono::milliseconds timeout = std::chrono::milliseconds(5000)) {
+    auto& injector = utils::get_local_injector();
+    constexpr auto sleep_duration = std::chrono::milliseconds(100);
+    std::chrono::milliseconds time_waited{0};
+    while (injector.enter_count(name) < count) {
+        if (time_waited > timeout) {
+            throw std::runtime_error(fmt::format("timeout reached when waiting for {}", name));
+        }
+        co_await sleep(sleep_duration);
+        time_waited += sleep_duration;
+    }
+}
+
+SEASTAR_THREAD_TEST_CASE(sstable_auto_scrub_corrupted_ssts_with_scylla_test) {
+    automatic_scrub_test_framework test(tests::random_schema_specification::compress_sstable::yes);
+
+    auto& test_env = test.env();
+    constexpr auto sst_count = 5;
+
+    test.run(sst_count, [&test_env] (table_for_tests& table, compaction::compaction_group_view& ts, std::vector<sstables::shared_sstable> sstables) {
+        auto& cm = test_env.test_compaction_manager();
+
+        for (sstables::shared_sstable& sst : sstables) {
+            sst->set_scrub_time(db_clock::from_time_t(0));
+            slightly_corrupt_sstable(sst);
+        }
+        
+        cm.set_scrub_period(std::chrono::seconds(3600));
+        cm.trigger_auto_scrub_timer();
+
+        wait_on_enter("automatic_scrub_compaction_done", sst_count).get();
+
+        BOOST_REQUIRE_EQUAL(table->get_sstables()->size(), sstables.size());
+        for (auto& sst : *table->get_sstables()) {
+            BOOST_REQUIRE(sst->is_quarantined());
+        }
+    });
+}
+
 } // namespace
